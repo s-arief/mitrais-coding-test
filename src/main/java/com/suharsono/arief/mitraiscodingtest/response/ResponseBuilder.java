b8302f962f6cf8f22d95c851629c63963c21136c
Arief Suharsono <ariefsuharsono@gmail.com>
2020-08-26 20:06:32 +0700

    response builder

---
+++ src/main/java/com/suharsono/arief/mitraiscodingtest/response/ResponseBuilder.java
@@ -0,0 +1,51 @@
+package com.suharsono.arief.mitraiscodingtest.response;
+
+import org.json.JSONObject;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+
+public class ResponseBuilder {
+    
+    // Error after calling controllers
+    public static ResponseEntity buildResponseBadRequest() {
+        JSONObject retVal = new JSONObject();
+        retVal.put("error", true);
+        retVal.put("code", ResponseMessage.GenericBadRequest.code);
+        retVal.put("message", ResponseMessage.GenericBadRequest.message);
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(retVal.toString());
+    }
+    
+    public static ResponseEntity buildResponseServerError() {
+        JSONObject retVal = new JSONObject();
+        retVal.put("error", true);
+        retVal.put("code", ResponseMessage.GenericServerError.code);
+        retVal.put("message", ResponseMessage.GenericServerError.message);
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(retVal.toString());
+    }
+    
+    public static ResponseEntity buildResponseError(ResponseMessage rm) {
+        JSONObject retVal = new JSONObject();
+        retVal.put("error", true);
+        retVal.put("code", rm.code);
+        retVal.put("message", rm.message);
+        return ResponseEntity.status(HttpStatus.OK).body(retVal.toString());
+    }
+    
+    public static ResponseEntity buildResponseError(ResponseException ex) {
+        JSONObject retVal = new JSONObject();
+        retVal.put("error", true);
+        retVal.put("code", ex.getResponseMessage().code);
+        retVal.put("message", ex.getResponseMessage().message);
+        return ResponseEntity.status(HttpStatus.OK).body(retVal.toString());
+    }
+    
+    // Success
+    public static ResponseEntity buildResponseSuccess(ResponseMessage rm) {
+        JSONObject retVal = new JSONObject();
+        retVal.put("error", false);
+        retVal.put("code", rm.code);
+        retVal.put("message", rm.message);
+        return ResponseEntity.status(HttpStatus.OK).body(retVal.toString());
+    }
+    
+}
