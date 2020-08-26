7b713fc749c557909329ebc15d9a1ec8e49f51c9
Arief Suharsono <ariefsuharsono@gmail.com>
2020-08-26 20:07:09 +0700

    handle invalid inputs

--- src/main/java/com/suharsono/arief/mitraiscodingtest/controller/RegistrationController.java
+++ src/main/java/com/suharsono/arief/mitraiscodingtest/controller/RegistrationController.java
@@ -1,14 +1,16 @@
 package com.suharsono.arief.mitraiscodingtest.controller;
 
 import com.suharsono.arief.mitraiscodingtest.model.Registration;
+import com.suharsono.arief.mitraiscodingtest.response.ResponseBuilder;
+import com.suharsono.arief.mitraiscodingtest.response.ResponseException;
+import com.suharsono.arief.mitraiscodingtest.response.ResponseMessage;
 import com.suharsono.arief.mitraiscodingtest.service.RegistrationService;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 import javax.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -24,22 +26,32 @@ public class RegistrationController {
             value = "/register",
             method = RequestMethod.POST,
             produces = MediaType.APPLICATION_JSON_VALUE)
-    public String register(@Valid @RequestBody RegisterRequest request) {
+    public ResponseEntity register(@Valid @RequestBody RegisterRequest request) {
         SimpleDateFormat sdf = new SimpleDateFormat("d-M-Y");
         
         Registration registration = new Registration();
+        String mobileNumber = request.getMobileNumber();
+        if (mobileNumber.length() > 14 ||
+                (mobileNumber.charAt(0) != '0') && !mobileNumber.substring(0, 2).equals("62")) {
+            return ResponseBuilder.buildResponseError(ResponseMessage.RegistrationMobileNumberInvalid);
+        }
+        
         registration.setMobileNumber(request.getMobileNumber());
         registration.setFirstName(request.getFirstName());
         registration.setLastName(request.getLastName());
         try {
             registration.setBirthDate(sdf.parse(request.getBirthDate()));
         } catch (ParseException ex) {
-            Logger.getLogger(RegistrationController.class.getName()).log(Level.SEVERE, null, ex);
+            return ResponseBuilder.buildResponseError(ResponseMessage.RegistrationBirthdateInvalid);
         }
         registration.setGender(request.getGender());
         registration.setEmail(request.getEmail());
-        registrationService.save(registration);
-        return "success";
+        try {
+            registrationService.save(registration);
+        } catch (ResponseException ex) {
+            return ResponseBuilder.buildResponseError(ex);
+        }
+        return ResponseBuilder.buildResponseSuccess(ResponseMessage.RegistrationSuccess);
     }
     
 }
--- src/main/java/com/suharsono/arief/mitraiscodingtest/service/RegistrationService.java
+++ src/main/java/com/suharsono/arief/mitraiscodingtest/service/RegistrationService.java
@@ -2,7 +2,10 @@ package com.suharsono.arief.mitraiscodingtest.service;
 
 import com.suharsono.arief.mitraiscodingtest.model.Registration;
 import com.suharsono.arief.mitraiscodingtest.repository.RegistrationRepository;
+import com.suharsono.arief.mitraiscodingtest.response.ResponseException;
+import com.suharsono.arief.mitraiscodingtest.response.ResponseMessage;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -11,8 +14,19 @@ public class RegistrationService {
     @Autowired
     private RegistrationRepository registrationRepository;
     
-    public void save(Registration registration) {
-        registrationRepository.save(registration);
+    public void save(Registration registration) throws ResponseException {
+        try {
+            registrationRepository.save(registration);
+        } catch (DataIntegrityViolationException ex) {
+            String message = ex.getMessage();
+            if (message.contains("constraint [mobile_number]")) {
+                throw new ResponseException(ResponseMessage.RegistrationMobileNumberAlreadyExist);
+            } else if (message.contains("constraint [email]")) {
+                throw new ResponseException(ResponseMessage.RegistrationEmailAlreadyExist);
+            } else {
+                throw new ResponseException(ResponseMessage.GenericDBError);
+            }
+        }
     }
     
 }
