cec891d1d95acda608f684c8a430037724d2ea28
Arief Suharsono <ariefsuharsono@gmail.com>
2020-08-27 02:07:42 +0700

    add methods for testing purpose

--- Backend/src/main/java/com/suharsono/arief/mitraiscodingtest/repository/RegistrationRepository.java
+++ Backend/src/main/java/com/suharsono/arief/mitraiscodingtest/repository/RegistrationRepository.java
@@ -1,10 +1,14 @@
 package com.suharsono.arief.mitraiscodingtest.repository;
 
 import com.suharsono.arief.mitraiscodingtest.model.Registration;
+import java.util.Set;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface RegistrationRepository extends CrudRepository<Registration, Integer> {
+    
+    public Set<Registration> findByMobileNumber(String mobileNumber);
+    public Set<Registration> findByEmail(String email);
     
 }
